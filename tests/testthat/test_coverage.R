609c492fc0fb542050f163b33725b4c103d1be36
andodet <an.dodet@gmail.com>
2021-09-16 19:55:14 +0200

    test: mock api calls with `{vcr}`
    Closes #17

---
+++ tests/testthat/test_coverage.R
@@ -0,0 +1,29 @@
+library(testthat)
+
+
+# Some valid coordinates
+coords <- c("41.899009,12.477243",
+            "41.889083,12.470514")
+
+
+test_that("check_coverage returns a `bool`", {
+  vcr::use_cassette("check_coverage", {
+    res <- check_coverage(coords[1])
+  })
+  expect_true(res)
+})
+
+
+test_that("Check coverage returnsa a list of bools", {
+  vcr::use_cassette("check_coverage_list", {
+    res <- check_coverage(coords)
+  })
+  expect_vector(res)
+})
+
+
+vcr::use_cassette("check_coverage_notoken", {
+  test_that("check_coverage errors out with no token", {
+    expect_error(check_coverage(coords, api_token = ""))
+  })
+})
